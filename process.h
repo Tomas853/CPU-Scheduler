055ff4a30222fc7098aaed6a6c848967453fe7b5
Tomas853 <tomas.gebrewold@wsu.edu>
2025-10-23 22:06:11 -0700

    finished sheduler project

--- process.h
+++ process.h
@@ -7,6 +7,8 @@
 #include <stdint.h>
 
 // Headers as needed
+#define TIME_QUANTUM 2
+
 
 typedef enum { false, true } bool;        // Allows boolean types in C
 
@@ -17,23 +19,71 @@ typedef struct Process {
     uint32_t C;                         // C: Total CPU time required
     uint32_t M;                         // M: Multiplier of CPU burst time
     uint32_t processID;                 // The process ID given upon input read
+    uint32_t randOffset;
 
     uint32_t remainingCPUTime;
     uint32_t currentCPUTimeRun;
     uint32_t currentIOBlockedTime;
     uint32_t currentWaitingTime;
     uint32_t finishingTime;
-    
-    uint32_t currentBurstRemaining;
+    uint32_t currentCPUBurst;
     uint32_t timeUntilUnblock;
-    uint32_t isBlocked;
-    uint32_t isRunning;
-    uint32_t isTerminated;
+    uint32_t remainingIOTime;
+
+    bool isBlocked;
+    bool isReady;
+    bool isRunning;
+    bool isFinished;
 } _process;
 
 uint32_t TOTAL_CREATED_PROCESSES = 0;   // The total number of processes constructed
+uint32_t TOTAL_FINISHED_PROCESSES = 0;
+uint32_t CURRENT_CYCLE = 0;
+uint32_t TOTAL_NUMBER_OF_CYCLES_SPENT_BLOCKED = 0;
 
-// Additional variables as needed
+// ======================Queue stuff======================
+typedef struct node {
+    _process* data;
+    struct node* next;
+} Node;
+
+typedef struct queue {
+    struct node* head;
+    struct node* tail;
+} Queue;
+
+
+void initQueue(Queue* q) {
+    q->head = q->tail = NULL;
+}
+
+bool isEmpty(Queue* q) {
+    return q->head == NULL;
+}
+
+void enqueue(Queue* q, _process* process) {
+    Node* newNode = malloc(sizeof(Node));
+    newNode->data = process;
+    newNode->next = NULL;
+
+    if (isEmpty(q)) {
+        q->head = q->tail = newNode;
+    }
+    else {
+        q->tail->next = newNode;
+        q->tail = newNode;
+    }
+}
+
+_process* dequeue(Queue* q) {
+    if (isEmpty(q)) return NULL;
+    Node* tmp = q->head;
+    _process* process_old = tmp->data;
+    q->head = q->head->next;
+    free(tmp);
+    return process_old;
+}
+//=============Queue stuff end =====================
 
 
 /**
@@ -64,11 +114,11 @@ uint32_t getRandNumFromFile(uint32_t line, FILE* random_num_file_ptr) {
  * Reads a random non-negative integer X from a file named random-numbers.
  * Returns the CPU Burst: : 1 + (random-number-from-file % upper_bound)
  */
-uint32_t randomOS(uint32_t upper_bound, uint32_t process_indx, FILE* random_num_file_ptr)
+uint32_t randomOS(uint32_t upper_bound, uint32_t offset,FILE* random_num_file_ptr)
 {
     char str[20];
 
-    uint32_t unsigned_rand_int = (uint32_t)getRandNumFromFile(SEED_VALUE + process_indx, random_num_file_ptr);
+    uint32_t unsigned_rand_int = (uint32_t)getRandNumFromFile(offset, random_num_file_ptr);
     uint32_t returnValue = 1 + (unsigned_rand_int % upper_bound);
 
     return returnValue;
@@ -177,6 +227,24 @@ void printSummaryData(_process process_list[])
     printf("\tAverage waiting time: %6f\n", avg_waiting_time);
 } // End of the print summary data function
 
+
+int compareByArrival(const void* a, const void* b) {
+    _process* p1 = (_process*)a;
+    _process* p2 = (_process*)b;
+
+    if (p1->A != p2->A) {
+        return p1->A - p2->A;
+    }
+
+    return p1->processID - p2->processID;
+}
+
+void sort_process_by_arrival(_process* process_list) {
+    qsort(process_list, TOTAL_CREATED_PROCESSES, sizeof(_process), compareByArrival);
+}
+
+
+
 _process* parse_input_file(const char* filename) {
     FILE* fp = fopen(filename, "r");
     if (!fp) {
@@ -205,15 +273,313 @@ _process* parse_input_file(const char* filename) {
 
         process_list[i].isBlocked = false;
         process_list[i].isRunning = false;
-        process_list[i].isTerminated = false;
+        process_list[i].isReady = false;
+        process_list[i].isFinished = false;
         process_list[i].currentCPUTimeRun = 0;
         process_list[i].currentIOBlockedTime = 0;
         process_list[i].currentWaitingTime = 0;
-        process_list[i].finishingTime = 0;
+        process_list[i].currentCPUBurst = 0;
+        process_list[i].timeUntilUnblock = 0;
+        process_list[i].remainingIOTime = 0;
+        process_list[i].randOffset = 0;
     }
 
     fclose(fp);
     return process_list;
 }
+
+void fcfsScheduler(_process* process_list, _process* finished_list, FILE* random_num_file_ptr, bool verbose) {
+    Queue ready_queue;
+    initQueue(&ready_queue);
+    _process* running_process = NULL;
+    uint32_t last_cpu_burst = 0;
+    while (TOTAL_FINISHED_PROCESSES < TOTAL_CREATED_PROCESSES) {
+        // 1. Unblock any finished I/O processes
+        for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+            _process* p = &process_list[i];
+            if (p->isBlocked) {
+                p->remainingIOTime--;
+                if (p->remainingIOTime == 0) {
+                    p->isBlocked = false;
+                    p->isReady = true;
+                    enqueue(&ready_queue, p);
+                    printf("process io at cycle %d\n", CURRENT_CYCLE);
+                }
+            }
+        }
+
+        // 2. Check for new arrivals at this cycle
+        for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+            _process* p = &process_list[i];
+            if (p->A == CURRENT_CYCLE && !p->isReady && !p->isFinished) {
+                p->isReady = true;
+                enqueue(&ready_queue, p);
+            }
+        }
+
+        // 3. If CPU is idle, schedule next ready process
+        if (!running_process && !isEmpty(&ready_queue)) {
+            running_process = dequeue(&ready_queue);
+            running_process->isReady = false;
+            running_process->isRunning = true;
+
+            uint32_t cpu_burst = randomOS(running_process->B, running_process->randOffset,random_num_file_ptr);
+            running_process->randOffset++;
+            if (cpu_burst > (running_process->C - running_process->currentCPUTimeRun)) {
+                cpu_burst = running_process->C - running_process->currentCPUTimeRun;
+            }
+            running_process->currentCPUBurst = cpu_burst;
+            last_cpu_burst = cpu_burst;
+        }
+
+        // 4. Run currently running process
+        if (running_process) {
+            running_process->currentCPUBurst--;
+            running_process->currentCPUTimeRun++;
+
+            if (running_process->currentCPUBurst == 0) {
+                if (running_process->currentCPUTimeRun == running_process->C) {
+                    running_process->finishingTime = CURRENT_CYCLE + 1;
+                    running_process->isFinished = true;
+                    running_process->isRunning = false;
+                    finished_list[TOTAL_FINISHED_PROCESSES++] = *running_process;
+                    running_process = NULL;
+                }
+                else {
+                    running_process->isBlocked = true;
+                    running_process->isRunning = false;
+                    running_process->remainingIOTime = running_process->M * last_cpu_burst;
+                    running_process = NULL;
+                }
+            }
+        }
+
+        // 5. Update waiting time for all ready processes
+        for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+            if (process_list[i].isReady) {
+                process_list[i].currentWaitingTime++;
+            }
+        }
+
+        // 6. Update I/O blocked time and global counter
+        bool anyBlocked = false;
+        for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+            if (process_list[i].isBlocked) {
+                process_list[i].currentIOBlockedTime++;
+                anyBlocked = true;
+            }
+        }
+        if (anyBlocked) {
+            TOTAL_NUMBER_OF_CYCLES_SPENT_BLOCKED++;
+        }
+
+        CURRENT_CYCLE++;
+    
+    };
+}
+
+void roundRobinScheduler(_process* process_list, _process* finished_list, FILE* random_num_file_ptr, bool verbose) {
+    Queue ready_queue;
+    initQueue(&ready_queue);
+    _process* running_process = NULL;
+
+    uint32_t last_cpu_burst = 0;
+    uint32_t quantumRemaining = TIME_QUANTUM;
+
+    while (TOTAL_FINISHED_PROCESSES < TOTAL_CREATED_PROCESSES) {
+
+        // 1. Unblock processes whose I/O is done
+        for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+            _process* p = &process_list[i];
+            if (p->isBlocked) {
+                p->remainingIOTime--;
+                if (p->remainingIOTime == 0) {
+                    p->isBlocked = false;
+                    p->isReady = true;
+                    enqueue(&ready_queue, p);
+                }
+            }
+        }
+
+        // 2. Check for new arrivals
+        for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+            _process* p = &process_list[i];
+            if (p->A == CURRENT_CYCLE && !p->isFinished && !p->isReady) {
+                p->isReady = true;
+                enqueue(&ready_queue, p);
+            }
+        }
+
+        // 3. If CPU is idle, pick new process
+        if (!running_process && !isEmpty(&ready_queue)) {
+            running_process = dequeue(&ready_queue);
+            running_process->isReady = false;
+            running_process->isRunning = true;
+
+            if (running_process->currentCPUBurst == 0) {
+                uint32_t cpu_burst = randomOS(running_process->B, running_process->randOffset, random_num_file_ptr);
+                running_process->randOffset++;
+                if (cpu_burst > (running_process->C - running_process->currentCPUTimeRun)) {
+                    cpu_burst = running_process->C - running_process->currentCPUTimeRun;
+                }
+                running_process->currentCPUBurst = cpu_burst;
+                last_cpu_burst = cpu_burst;
+            }
+
+            quantumRemaining = TIME_QUANTUM;
+        }
+
+        // 4. Execute running process
+        if (running_process) {
+            running_process->currentCPUBurst--;
+            running_process->currentCPUTimeRun++;
+            quantumRemaining--;
+
+            if (running_process->currentCPUTimeRun == running_process->C) {
+                // Finished
+                running_process->finishingTime = CURRENT_CYCLE + 1;
+                running_process->isFinished = true;
+                running_process->isRunning = false;
+                finished_list[TOTAL_FINISHED_PROCESSES++] = *running_process;
+                running_process = NULL;
+            }
+            else if (running_process->currentCPUBurst == 0) {
+                // Needs I/O
+                running_process->isBlocked = true;
+                running_process->isRunning = false;
+                running_process->remainingIOTime = running_process->M * last_cpu_burst;
+                running_process = NULL;
+            }
+            else if (quantumRemaining == 0) {
+                // Preempt
+                running_process->isRunning = false;
+                running_process->isReady = true;
+                enqueue(&ready_queue, running_process);
+                running_process = NULL;
+            }
+        }
+
+        // 5. Update waiting time
+        for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+            if (process_list[i].isReady) {
+                process_list[i].currentWaitingTime++;
+            }
+        }
+
+        // 6. Update I/O blocked time and global tracker
+        bool anyBlocked = false;
+        for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+            if (process_list[i].isBlocked) {
+                process_list[i].currentIOBlockedTime++;
+                anyBlocked = true;
+            }
+        }
+        if (anyBlocked) {
+            TOTAL_NUMBER_OF_CYCLES_SPENT_BLOCKED++;
+        }
+
+        CURRENT_CYCLE++;
+    }
+}
+
+void sjfScheduler(_process* process_list, _process* finished_list, FILE* random_num_file_ptr, bool verbose) {
+    _process* running_process = NULL;
+    uint32_t last_cpu_burst = 0;
+
+    while (TOTAL_FINISHED_PROCESSES < TOTAL_CREATED_PROCESSES) {
+        // 1. Unblock finished I/O
+        for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+            _process* p = &process_list[i];
+            if (p->isBlocked) {
+                p->remainingIOTime--;
+                if (p->remainingIOTime == 0) {
+                    p->isBlocked = false;
+                    p->isReady = true;
+                }
+            }
+        }
+
+        // 2. Check for arrivals
+        for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+            _process* p = &process_list[i];
+            if (p->A == CURRENT_CYCLE && !p->isFinished && !p->isReady) {
+                p->isReady = true;
+            }
+        }
+
+        // 3. If CPU idle, pick ready process with shortest remaining job
+        if (!running_process) {
+            _process* shortest = NULL;
+            for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+                _process* p = &process_list[i];
+                if (p->isReady && !p->isFinished) {
+                    uint32_t remaining = p->C - p->currentCPUTimeRun;
+                    if (!shortest || remaining < (shortest->C - shortest->currentCPUTimeRun)) {
+                        shortest = p;
+                    }
+                }
+            }
+
+            if (shortest) {
+                running_process = shortest;
+                running_process->isReady = false;
+                running_process->isRunning = true;
+
+                uint32_t cpu_burst = randomOS(running_process->B, running_process->randOffset, random_num_file_ptr);
+                running_process->randOffset++;
+                if (cpu_burst > (running_process->C - running_process->currentCPUTimeRun)) {
+                    cpu_burst = running_process->C - running_process->currentCPUTimeRun;
+                }
+                running_process->currentCPUBurst = cpu_burst;
+                last_cpu_burst = cpu_burst;
+            }
+        }
+
+        // 4. Execute current process
+        if (running_process) {
+            running_process->currentCPUBurst--;
+            running_process->currentCPUTimeRun++;
+
+            if (running_process->currentCPUTimeRun == running_process->C) {
+                // Done
+                running_process->finishingTime = CURRENT_CYCLE + 1;
+                running_process->isFinished = true;
+                running_process->isRunning = false;
+                finished_list[TOTAL_FINISHED_PROCESSES++] = *running_process;
+                running_process = NULL;
+            }
+            else if (running_process->currentCPUBurst == 0) {
+                // Needs I/O
+                running_process->isBlocked = true;
+                running_process->isRunning = false;
+                running_process->remainingIOTime = running_process->M * last_cpu_burst;
+                running_process = NULL;
+            }
+        }
+
+        // 5. Waiting time for ready processes
+        for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+            if (process_list[i].isReady) {
+                process_list[i].currentWaitingTime++;
+            }
+        }
+
+        // 6. I/O time
+        bool anyBlocked = false;
+        for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+            if (process_list[i].isBlocked) {
+                process_list[i].currentIOBlockedTime++;
+                anyBlocked = true;
+            }
+        }
+        if (anyBlocked) {
+            TOTAL_NUMBER_OF_CYCLES_SPENT_BLOCKED++;
+        }
+
+        CURRENT_CYCLE++;
+    }
+}
+
+
 
 #endif // !PROCESS_H
--- scheduler.c
+++ scheduler.c
@@ -1,17 +1,102 @@
 #include "process.h"
-#include "queue.h"
 
 /**
  * The magic starts from here
  */
 int main(int argc, char *argv[])
 {
-    uint32_t total_num_of_process;               // Read from the file -- number of process to create
-    _process process_list[total_num_of_process]; // Creates a container for all processes
-    // Other variables
-    printf("yello");
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
 
-    // Write code for your shiny scheduler
+    bool verbose = false;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--verbose") == 0 || strcmp(argv[i], "-v") == 0) {
+            verbose = true;
+        }
+    }
+
+    _process* process_list = parse_input_file(argv[1]);
+    _process* finished_list = malloc(sizeof(_process) * TOTAL_CREATED_PROCESSES);
+    sort_process_by_arrival(process_list); //sort processes by arrival
+
+    FILE* random_num_file_ptr = fopen("random-numbers", "r");
+    if (!random_num_file_ptr) {
+        perror("Failed to open random-number file");
+        exit(1);
+    }
+
+    printf("######################### START OF FIRST COME FIRST SERVE #########################\n");
+    printStart(process_list);
+    fcfsScheduler(process_list, finished_list, random_num_file_ptr,verbose);
+    
+    
+    printFinal(process_list);
+    printProcessSpecifics(process_list);
+    printSummaryData(process_list);
+    printf("######################### END OF FIRST COME FIRST SERVE #########################\n");
+
+    printf("######################### START OF ROUND ROBIN #########################\n");
+    TOTAL_FINISHED_PROCESSES = 0;
+    CURRENT_CYCLE = 0;
+    TOTAL_NUMBER_OF_CYCLES_SPENT_BLOCKED = 0;
+    //reset
+    for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+        _process* p = &process_list[i];
+        p->isReady = false;
+        p->isBlocked = false;
+        p->isRunning = false;
+        p->isFinished = false;
+        p->currentCPUTimeRun = 0;
+        p->currentIOBlockedTime = 0;
+        p->currentWaitingTime = 0;
+        p->remainingIOTime = 0;
+        p->currentCPUBurst = 0;
+        p->finishingTime = 0;
+        p->randOffset = 0;
+    }
+    printStart(process_list);
+    roundRobinScheduler(process_list, finished_list, random_num_file_ptr, true);
+    printFinal(finished_list);
+    printProcessSpecifics(finished_list);
+    printSummaryData(finished_list);
+    printf("######################### END OF ROUND ROBIN #########################\n");
+
+    printf("######################### START OF SHORTEST JOB FIRST #########################\n");
+    TOTAL_FINISHED_PROCESSES = 0;
+    CURRENT_CYCLE = 0;
+    TOTAL_NUMBER_OF_CYCLES_SPENT_BLOCKED = 0;
+    //reset
+    for (int i = 0; i < TOTAL_CREATED_PROCESSES; i++) {
+        _process* p = &process_list[i];
+        p->isReady = false;
+        p->isBlocked = false;
+        p->isRunning = false;
+        p->isFinished = false;
+        p->currentCPUTimeRun = 0;
+        p->currentIOBlockedTime = 0;
+        p->currentWaitingTime = 0;
+        p->remainingIOTime = 0;
+        p->currentCPUBurst = 0;
+        p->finishingTime = 0;
+        p->randOffset = 0;
+    }
+    printStart(process_list);
+    sjfScheduler(process_list, finished_list, random_num_file_ptr, verbose);
+    printFinal(finished_list);
+    printProcessSpecifics(finished_list);
+    printSummaryData(finished_list);
+    printf("######################### END OF SHORTEST JOB FIRST #########################\n");
+
+    
+    
+    
+
+
+    free(process_list);
+    free(finished_list);
+    fclose(random_num_file_ptr);
 
     return 0;
 } 
